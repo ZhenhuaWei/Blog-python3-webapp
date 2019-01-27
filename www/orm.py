8623e0afd72b15e350bef4bf2ad52f30bffc7f47
weizhenhua-deepin <weizhenhua94@163.com>
2019-01-27 23:34:47 +0800

    1. add coroweb file 2. rewrite app.py file 3. app.py can not run, SyntaxError: 'yield from' inside async function error.

--- www/orm.py
+++ www/orm.py
@@ -5,88 +5,87 @@ import asyncio, logging
 import aiomysql
 
 def log(sql, args=()):
-	logging.info("SQL:%s" %sql)
+    logging.info("SQL:%s" %sql)
 
 
 async def create_pool(loop, **kw):
-	logging.info("create database connection pool...")
-	global __pool
-
-	__pool = await aiomysql.create_pool(
-		host=kw.get("host", "localhost"),
-		port=kw.get("port", 3306),
-		user=kw["user"],
-		password=kw["password"],
-		db=kw["db"],
-		charset=kw.get("charset", "utf-8"),
-		autocommit=kw.get("autocommit", True),
-		maxsize=kw.get("maxsize", 10),
-		minsize=kw.get("minsize",1),
-		loop=loop
-		)
+    logging.info("create database connection pool...")
+    global __pool
+    __pool = yield from aiomysql.create_pool(
+        host=kw.get('host', 'localhost'),
+        port=kw.get('port', 3306),
+        user=kw['user'],
+        password=kw['password'],
+        db=kw['db'],
+        charset=kw.get('charset', 'utf8'),
+        autocommit=kw.get('autocommit', True),
+        maxsize=kw.get('maxsize', 10),
+        minsize=kw.get('minsize', 1),
+        loop=loop
+        )
 
 async def select(sql, args,size=None):
-	log(sql, args)
+    log(sql, args)
 
-	global __pool
+    global __pool
 
-	async with __pool.get() as conn:
-		async with conn.cursor(aiomysql.DectCursor) as cur:
-			await cur.execute(sql.replace('?','%s'), args or ())
-			if size:
-				rs = await cur.fetchmany(size)
-			else:
-				rs = await cur.fetchall()
-		logging.info("rows returned:%s" %len(rs))
-		return rs
+    async with __pool.get() as conn:
+        async with conn.cursor(aiomysql.DectCursor) as cur:
+            await cur.execute(sql.replace('?','%s'), args or ())
+            if size:
+                rs = await cur.fetchmany(size)
+            else:
+                rs = await cur.fetchall()
+        logging.info("rows returned:%s" %len(rs))
+        return rs
 
 async def execute(sql, args, autocommit=True):
-	log(sql)
-	async with __pool.get() as conn:
-		if not autocommit:
-			await conn.begin()
-		try:
-			async with conn.cursor(aiomysql.DectCursor) as cur:
-				await cur.execute(sql.replace("?","%s"), args)
-				affected = cur.rowcount
-			if not autocommit:
-				await conn.commit()
-		except BaseException as e:
-			if not autocommit:
-				await conn.rollback()
-			raise
-		return affected
+    log(sql)
+    async with __pool.get() as conn:
+        if not autocommit:
+            await conn.begin()
+        try:
+            async with conn.cursor(aiomysql.DectCursor) as cur:
+                await cur.execute(sql.replace("?","%s"), args)
+                affected = cur.rowcount
+            if not autocommit:
+                await conn.commit()
+        except BaseException as e:
+            if not autocommit:
+                await conn.rollback()
+            raise
+        return affected
 
 def create_args_string(num):
-	L = []
-	for n in range(num):
-		L.append("?")
+    L = []
+    for n in range(num):
+        L.append("?")
 
-	return ', '.join(L)
+    return ', '.join(L)
 
 
 class Field(object):
- 	"""docstring for Field"""
- 	def __init__(self, name, column_type, primary_key, default):
- 		self.name = name
- 		self.column_type = column_type
- 		self.primary_key = primary_key
+    """docstring for Field"""
+    def __init__(self, name, column_type, primary_key, default):
+        self.name = name
+        self.column_type = column_type
+        self.primary_key = primary_key
 
- 		self.default = default
+        self.default = default
 
- 	def __str__(self):
- 		return '<%s, %s:%s>' %(self.__class__.__name__, self.column_type, self.name)
+    def __str__(self):
+        return '<%s, %s:%s>' %(self.__class__.__name__, self.column_type, self.name)
 
 
 class StringField(Field):
     def __init__(self, name=None, primary_key=False, default=None, ddl='varchar(100)'):
         super().__init__(name, ddl, primary_key, default)
-		
+
 
 class BooleanField(Field):
     def __init__(self, name=None, default=False):
         super().__init__(name, 'boolean', False, default)
-		
+
 class IntegerField(Field):
     def __init__(self, name=None, primary_key=False, default=0):
         super().__init__(name, 'bigint', primary_key, default)
